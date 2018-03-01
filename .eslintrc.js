4170bbb69babb29998db70b58e329167c6f4a19c
Dmitriy-Kiselyov <dmitriy.penetrator@gmail.com>
2018-03-02 01:17:26 +0300

    Ultra refactoring + use webpack

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    extends: 'gemini-testing',
+    root: true,
+    globals: {
+        performance: false
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    entry: './lib/static/genetic.js',
+    output: {
+        filename: 'bundle.js',
+        path: __dirname
+    }
+};
