89be55023bc873674a825bb902e3a4ecab693656
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-04-21 18:18:39 +0300

    Preferences + refactor Selects

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,9 @@ module.exports = {
     extends: 'gemini-testing',
     root: true,
     globals: {
-        performance: false
+        performance: false,
+        localStorage: false,
+        document: false
     },
     rules: {
         "linebreak-style": "off"
