1722891bdebed2c897e5e794441ce20a581a0f1f
Dmitriy-kiselyov <dmitriy.penetrator@gmail.com>
2018-05-13 20:10:03 +0300

    Multiple TSP game!

--- test/genetic/game/logger.js
+++ test/genetic/game/logger.js
@@ -4,25 +4,25 @@ const {printNumber} = require('../../../lib/react/modules/utils');
 
 module.exports = (game) => {
     game.on('init', ({index, vertex}) => {
-        console.log(`Стартовая вершина игрока №${index}: ${vertex}`);
+        console.log(`Стартовая вершина игрока №${index + 1}: ${vertex}`);
     });
 
-    game.on('turn', ({index, vertex, time}) => {
-        console.log(`Игрок №${index} выбрал вершину ${vertex}, придёт в нее в ${printNumber(time, 2)}`);
+    game.on('turn', ({index, next, time}) => {
+        console.log(`Игрок №${index + 1} выбрал вершину ${next}, придёт в нее в ${printNumber(time, 2)}`);
     });
 
-    game.on('disqualify', ({state, index, vertex}) => {
+    game.on('disqualify', ({state, index, next}) => {
         switch (state) {
             case 'init':
-                console.log(`Игрок №${index} вернул неверный номер вершины и отстранён от игры!`);
+                console.log(`Игрок №${index + 1} вернул неверный номер вершины: ${next} и отстранён от игры!`);
                 return;
             case 'turn':
-                console.log(`Игрок №${index} вернул вершину №${vertex}, которая уже была посещена, и остранён от игры!`);
+                console.log(`Игрок №${index + 1} вернул вершину №${next}, которая уже была посещена, и остранён от игры!`);
         }
     });
 
     game.on('result', ({index, tour}) => {
-        console.log(`Результаты игрока №${index}:`);
+        console.log(`Результаты игрока №${index + 1}:`);
         console.log(`Пройденный путь: ${tour.toString()}`);
         console.log(`Пройденное расстояние: ${printNumber(tour.distance, 2)}`);
         console.log('----------------------------------');
